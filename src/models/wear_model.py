b70a8a6c0a1c1ec3be62306072708bb30e6c98f1
xcodesgit <anishgoel@berkeley.edu>
2025-01-15 17:54:45 -0800

    added a model to take wear and tear into account

---
+++ src/models/wear_model.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calculate_tool_wear(cutting_speed, material_hardness, temperature, time_points):
+    """
+    wear and tear over time based on cutting parameters
+    """
+    wear_rate = (
+        0.001 * cutting_speed *       #scaling factor that can be adjustable to how durable machine is 
+        (material_hardness / 100) * 
+        (temperature / 50)
+    )
+    return np.cumsum(wear_rate * np.ones_like(time_points))  # taking cumulative sum as new wear tear is add on to previous wear and tear
+                                                              # assuming, there is no change or fixing or changing parts during the time points
